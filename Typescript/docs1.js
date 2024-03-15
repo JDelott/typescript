de587d1dd557a24d5c1b0a93fa3dacc2078dfd26
Jacob Delott <jldelott@gmail.com>
2024-03-15 10:10:27 -0700

    more docs study

---
+++ Typescript/docs1.js
@@ -0,0 +1,9 @@
+// interface User{
+//     name: string;
+//     id: number;
+// }
+function logPoint(p) {
+    console.log("".concat(p.x, ",").concat(p.y));
+}
+var point = { x: 12, y: 26 };
+logPoint(point);
--- Typescript/docs1.ts
+++ Typescript/docs1.ts
@@ -1,65 +1,93 @@
-interface User{
-    name: string;
-    id: number;
-}
+// interface User{
+//     name: string;
+//     id: number;
+// }
 
 
-class UserAccount{
-    name: string;
-    id: number;
+// class UserAccount{
+//     name: string;
+//     id: number;
 
-    constructor(name: string, id:number){
-        this.name = name;
-        this.id
-    }
-}
+//     constructor(name: string, id:number){
+//         this.name = name;
+//         this.id
+//     }
+// }
 
-const user: User = new UserAccount("Jacob",4)
+// const user: User = new UserAccount("Jacob",4)
 
-// const user: User = {
-//     name: "Jacob",
-//     id: 4
+// // const user: User = {
+// //     name: "Jacob",
+// //     id: 4
+// // }
+
+
+
+// // UNIONS TYPES 
+
+// type MyBool = true | false
+
+// type WindowStates = "open" | "closed" | "minimized";
+// type LockStates = "locked" | "unlocked";
+// type PositiveNumbersUnderTen = 1 | 3 | 5 |7 | 9;
+
+// function getLength(obj: string | string[]) {
+//     return obj.length;
 // }
 
 
 
-// UNIONS TYPES 
+// // Typeof
 
-type MyBool = true | false
 
-type WindowStates = "open" | "closed" | "minimized";
-type LockStates = "locked" | "unlocked";
-type PositiveNumbersUnderTen = 1 | 3 | 5 |7 | 9;
+// function wrapInArray(obj: string | string[]) {
+//     if (typeof obj === "string") {
+//         return [obj];
+//     }
+//     return obj;
+// } 
 
-function getLength(obj: string | string[]) {
-    return obj.length;
+// // Generics
+
+
+// type  StringArray = Array<string>;
+// type NumberArray = Array<number>;
+// type ObjectWithNameArray = Array<{name: string}>;
+
+
+// interface Backpack<Type>{
+//     add:(obj: Type) => void;
+//     get: () => Type;
+// }
+
+// declare const backpack: Backpack<string>
+// const object = backpack.get();
+// backpack.add("23");
+
+
+interface Point {
+    x: number;
+    y: number;
+
 }
 
+function logPoint(p:Point) {
+    console.log(`${p.x},${p.y}`);
+}
+
+const point = {x:12, y:26};
+logPoint(point);
 
 
-// Typeof
+class VirtualPoint {
+    x:number;
+    y:number;
 
-
-function wrapInArray(obj: string | string[]) {
-    if (typeof obj === "string") {
-        return [obj];
+    constructor(x:number, y:number){
+        this.x = x;
+        this.y = y;
     }
-    return obj;
-} 
-
-// Generics
-
-
-type  StringArray = Array<string>;
-type NumberArray = Array<number>;
-type ObjectWithNameArray = Array<{name: string}>;
-
-
-interface Backpack<Type>{
-    add:(obj: Type) => void;
-    get: () => Type;
 }
 
-declare const backpack: Backpack<string>
-const object = backpack.get();
-backpack.add("23");
+const newVPoint = new VirtualPoint(13,56);
+logPoint(newVPoint);
